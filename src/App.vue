21499e07c2d70415e964be5cac8ab6c9066d3247
AntoineLsp <toto5100@users.noreply.github.com>
2019-10-28 12:54:42 +0100

    Cleanup and structuration en components

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,30 @@
 <template>
   <v-app>
-    <v-app-bar app>
-      <v-toolbar-title class="headline text-uppercase" dark color="primary">
+    <v-app-bar app dark color="blue">
+      <v-toolbar-title class="headline text-uppercase">
         <span>Trouve ton projet</span>
       </v-toolbar-title>
       <v-spacer></v-spacer>
-      <v-btn class="pink with white--text">Sign in</v-btn>
-      <!--<v-btn
-        text
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-      >
-        <span class="mr-2">Latest Release</span>
-      </v-btn>
-      -->
+      <v-btn class="cyan with white--text">Inscription</v-btn>
+      <v-btn class="cyan with white--text">Connexion</v-btn>
     </v-app-bar>
 
     <v-content>
-      <HelloWorld/>
-      Ce site à pour but de faciliter la recheche et le partage de projet.
-      <br>Si tu souhaites joindre un projet, ou proposer tes idées, connectes toi sans plus attendre.
+      <Welcome/>
       <Login/>
     </v-content>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
+import Welcome from './components/Welcome'
 import Login from './components/Login'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld,
-    Login
+    Login,
+    Welcome
   },
   data: () => ({
     //
---
+++ src/components/Welcome.vue
@@ -0,0 +1,7 @@
+<template>
+  <v-container>
+    <v-layout align-center justify-center>
+        Tu souhaites trouver le PST de ton année ?
+    </v-layout>
+  </v-container>
+</template>
