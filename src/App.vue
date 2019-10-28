50b82e5a0cd9d2ed8e4f0a063c22869d52d8b6d5
AntonLsp <32941176+AntonLsp@users.noreply.github.com>
2019-10-28 13:31:12 +0100

    Diaporama sur la page d'accueil

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@
         <span>Trouve ton projet</span>
       </v-toolbar-title>
       <v-spacer></v-spacer>
-      <v-btn class="cyan with white--text">Inscription</v-btn>
+      <v-btn class="mr-5 cyan with white--text">Inscription</v-btn>
       <v-btn class="cyan with white--text">Connexion</v-btn>
     </v-app-bar>
   
--- src/components/Welcome.vue
+++ src/components/Welcome.vue
@@ -1,8 +1,30 @@
 <template>
   <v-container>
     <v-layout align-center justify-center>
-        Tu souhaites trouver le PST de ton année ?
-        C'est ici.
+      <v-carousel cycle height="400" hide-delimiter-background show-arrows-on-hover>
+        <v-carousel-item v-for="(slide, i) in slides" :key="i">
+          <v-sheet :color="colors[i]" height="100%">
+            <v-row class="fill-height" align="center" justify="center">
+              <div class="display-3">{{ slide }}</div>
+            </v-row>
+          </v-sheet>
+        </v-carousel-item>
+      </v-carousel>
     </v-layout>
   </v-container>
 </template>
+
+<script>
+export default {
+  data () {
+    return {
+      colors: [
+        'cyan',
+        'green darken-2',
+        'red lighten-1'
+      ],
+      slides: ['Un répertoire de PST', 'Des tonnes de PST intéressants', 'Adaptés à tes goûts']
+    }
+  }
+}
+</script>
