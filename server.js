bdfdccc97d01b2cbc6acd9f9f56d0b4035c2862c
AntonLsp <32941176+AntonLsp@users.noreply.github.com>
2019-10-30 20:22:47 +0100

    Intégration du système de projets

--- server.js
+++ server.js
@@ -18,12 +18,14 @@ app.use(cors({
   credentials: true,
   origin: 'http://localhost:8080'
 }))
+
 app.use(session({
-  secret: 'secret123', // changez cette valeur
+  secret: 'acakolials14', // changez cette valeur
   resave: false,
   saveUninitialized: true,
   cookie: { secure: false } // ne changez que si vous avez activé le https
 }))
+
 app.use(morgan('dev'))
 app.use(bodyParser.json())
 
@@ -34,6 +36,45 @@ const users = [{
   username: 'admin',
   password: '1234'
 }]
+
+let projects = [
+  {
+    name: 'Projet WEB',
+    creator: 'Teacher',
+    description: 'Building a website with modern tooling and understanding of front-end and back-end concepts\nFront-end technologies: HTML5, CSS3, Javascript (Vue.js)\nBack-end technologies: Node.js (Express)',
+    tags: ['html', 'css', 'web']
+  },
+  {
+    name: 'Projet WEB1',
+    creator: 'Teacher',
+    description: 'Building a website with modern tooling and understanding of front-end and back-end concepts\nFront-end technologies: HTML5, CSS3, Javascript (Vue.js)\nBack-end technologies: Node.js (Express)',
+    tags: ['html', 'css', 'web']
+  },
+  {
+    name: 'Projet WEB2',
+    creator: 'Teacher',
+    description: 'Building a website with modern tooling and understanding of front-end and back-end concepts\nFront-end technologies: HTML5, CSS3, Javascript (Vue.js)\nBack-end technologies: Node.js (Express)',
+    tags: ['html', 'css', 'web']
+  },
+  {
+    name: 'Projet WEB3',
+    creator: 'Teacher',
+    description: 'Building a website with modern tooling and understanding of front-end and back-end concepts\nFront-end technologies: HTML5, CSS3, Javascript (Vue.js)\nBack-end technologies: Node.js (Express)',
+    tags: ['html', 'css', 'web']
+  },
+  {
+    name: 'Projet WEB4',
+    creator: 'Teacher',
+    description: 'Building a website with modern tooling and understanding of front-end and back-end concepts\nFront-end technologies: HTML5, CSS3, Javascript (Vue.js)\nBack-end technologies: Node.js (Express)',
+    tags: ['html', 'css', 'web']
+  },  {
+    name: 'Projet WEB5',
+    creator: 'Teacher',
+    description: 'Building a website with modern tooling and understanding of front-end and back-end concepts\nFront-end technologies: HTML5, CSS3, Javascript (Vue.js)\nBack-end technologies: Node.js (Express)',
+    tags: ['html', 'css', 'web']
+  }
+  
+]
 
 app.post('/api/login', (req, res) => {
   console.log('req.body', req.body)
@@ -99,6 +140,37 @@ app.get('/api/logout', (req, res) => {
     req.session.userId = 0
     res.json({
       message: 'disconnected'
+    })
+  }
+})
+
+function getMatchingProjects (tags) {
+  let matchingProj = []
+  // Pour chaque tag demandé
+  tags.forEach(tag => {
+    // Pour chaque projet dans la liste
+    projects.forEach(project => {
+      // Si le projet contient au moins un des tags, alors on l'ajoute aux matchs
+      if (!matchingProj.includes(project) && project.tags.includes(tag)) {
+        matchingProj.push(project)
+      }
+    })
+  })
+  return matchingProj
+}
+
+app.post('/api/getprojects', (req, res) => {
+  console.log('project req.body', req.body)
+  console.log('project req.query', req.query)
+  if (req.session.userId) {
+    res.json({
+      matchingProjects: getMatchingProjects(req.body.data.tags),
+      message: 'ok'
+    })
+  } else {
+    req.session.userId = 0
+    res.json({
+      message: 'not connected'
     })
   }
 })
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -6,6 +6,7 @@
     <v-layout align-center justify-center>
       Ces projets pourraient t'intéresser :
     </v-layout>
+    
   </v-container>
 </template>
 
@@ -13,14 +14,42 @@
 export default {
   name: 'Home',
   props: {
-    username:{
+    username: {
       type: String,
       default: '[USER]'
     }
   },
-  data () {
-    return {
-      color: 'red'
+  data: () => ({
+    color: 'red',
+    projects: [],
+    model: '',
+    description: ''
+  }),
+  created: function () {
+    setTimeout(this.getProjects(), 500)
+  },
+  methods: {
+
+    getProjects () {
+      let data = {
+        tags: ['web', 'c']
+      }
+      this.axios.post('http://localhost:4000/api/getprojects', {
+        data: data
+      })
+        .then((response) => {
+          if (response.data.message === 'ok') {
+            this.projects = response.data.matchingProjects
+            console.log(response.data.matchingProjects)
+          } else {
+            // Si on ne peut pas charger de projet, alors on retourne sur la page welcome
+            this.$router.push('Welcome')
+          }
+        })
+    },
+    toggle () {
+      console.log('sdqfsdf')
+      this.description = 'qsfdqsdf'
     }
   }
 }
